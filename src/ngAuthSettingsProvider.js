66bb608763f05c6e560de8af5d529a6dc37a8886
Michelle Darlea <mdarlea@gmail.com>
2015-07-05 12:56:52 -0400

    Added the ngAuthSettings provider

--- src/ngAuthSettingsProvider.js
+++ src/ngAuthSettingsProvider.js
@@ -1,17 +1,16 @@
 ﻿(function () {
     'use strict';
-    
+
     /**
     * @ngdoc service
     * @name swCommon.ngAuthSettingsProvider  
-    * @description ngAuthSettings provider
+    * @description {@link swCommon.ngAuthSettings ngAuthSettings} provider
     */ 
 
     /**
     * @ngdoc service
     * @name swCommon.ngAuthSettings    
-    * @description Provider that 
-    *
+    * @description Provides application configuration   for different environments such as: Dev, QA, Production
     */
     angular.module('swCommon').provider('ngAuthSettings', function () {
         var settings = {};
@@ -23,8 +22,28 @@
         this.$get = [
             function () {
                 var options = $.extend(true, {
+                    /**
+                    * @ngdoc property
+                    * @name swCommon.ngAuthSettings#indexPage
+                    * @propertyOf swCommon.ngAuthSettings
+                    * @returns {string} Path to the home (index) page. <br/><i>(Default: '/home')</i>
+                    */
                     indexPage: '/home',
+                    
+                    /**
+                    * @ngdoc property
+                    * @name swCommon.ngAuthSettings#apiServiceBaseUri
+                    * @propertyOf swCommon.ngAuthSettings
+                    * @returns {string} Application URL. Example: 'http://www.swaksoft.com/'. <br/><i>(Default: 'http://')</i>
+                    */
                     apiServiceBaseUri: 'http://',
+                    
+                    /**
+                    * @ngdoc property
+                    * @name swCommon.ngAuthSettings#clientId
+                    * @propertyOf swCommon.ngAuthSettings
+                    * @returns {string} The application name. <br/><i>(Default: 'swCommon')</i>
+                    */
                     clientId: 'swCommon'
                 }, settings);
                 
