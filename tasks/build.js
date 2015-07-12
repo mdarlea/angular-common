110bb1c865ee719c26fff581bc5a1b68495c3ff9
Michelle Darlea <mdarlea@gmail.com>
2015-07-12 16:30:08 -0400

    Added build task to the tasks folder

---
+++ tasks/build.js
@@ -0,0 +1,218 @@
+﻿'use strict';
+
+var markdown = require('node-markdown').Markdown;
+
+module.exports = function (grunt) {
+    grunt.registerTask('makeModuleMappingFile', function () {
+        var _ = grunt.util._;
+        var moduleMappingJs = 'dist/assets/module-mapping.json';
+        var moduleMappings = grunt.config('moduleFileMapping');
+        var moduleMappingsMap = _.object(_.pluck(moduleMappings, 'name'), moduleMappings);
+        var jsContent = JSON.stringify(moduleMappingsMap);
+        grunt.file.write(moduleMappingJs, jsContent);
+        grunt.log.writeln('File ' + moduleMappingJs.cyan + ' created.');
+    });
+
+    grunt.registerTask('build', 'Create build files', function () {
+        var _ = grunt.util._;
+        
+        //If arguments define what modules to build, build those. Else, everything
+        if (this.args.length) {
+            this.args.forEach(findModule);
+        } else {
+            grunt.file.expand({ filter: 'isDirectory', cwd: '.' }, 'src/*')
+            .forEach(function(dir) {
+                findModule(dir.split('/')[1]);
+            });
+        }
+        
+        var modules = grunt.config('build.modules');
+        grunt.config('srcModules', _.pluck(modules, 'moduleName'));
+        grunt.config('tplModules', _.pluck(modules, 'tplModules').filter(function (tpls) { return tpls.length > 0; }));
+        grunt.config('demoModules', modules.filter(function (module) {
+            return module.docs.md && module.docs.js && module.docs.html;
+        }).sort(function (a, b) {
+            if (a.name < b.name) { return -1; }
+            if (a.name > b.name) { return 1; }
+            return 0;
+        }));
+        
+        var cssStrings = _.flatten(_.compact(_.pluck(modules, 'css')));
+        var cssJsStrings = _.flatten(_.compact(_.pluck(modules, 'cssJs')));
+        if (cssStrings.length) {
+            grunt.config('meta.cssInclude', cssJsStrings.join('\n'));
+            
+            grunt.file.write(grunt.config('meta.cssFileDest'), grunt.config('meta.cssFileBanner') +
+                       cssStrings.join('\n'));
+            
+            grunt.log.writeln('File ' + grunt.config('meta.cssFileDest') + ' created');
+        }
+        
+        var moduleFileMapping = _.clone(modules, true);
+        moduleFileMapping.forEach(function (module) {
+            delete module.docs;
+        });
+        
+        grunt.config('moduleFileMapping', moduleFileMapping);
+        
+        var srcFiles = _.pluck(modules, 'srcFiles');
+        var tpljsFiles = _.pluck(modules, 'tpljsFiles');
+        //Set the concat task to concatenate the given src modules
+        grunt.config('concat.dist.src', grunt.config('concat.dist.src')
+                 .concat(srcFiles));
+        //Set the concat-with-templates task to concat the given src & tpl modules
+        grunt.config('concat.dist_tpls.src', grunt.config('concat.dist_tpls.src')
+                 .concat(srcFiles).concat(tpljsFiles));
+        
+        grunt.task.run(['concat', 'uglify', 'makeModuleMappingFile']);
+    });
+
+    //Common ui.bootstrap module containing all modules for src and templates
+    //findModule: Adds a given module to config
+    var foundModules = {};
+    var prefix = 'sw';
+    var modulePrefix = 'sw';
+    
+    function findModule(name) {
+        var modName = toAttribute(name);
+        
+        if (foundModules[modName]) { return; }
+        foundModules[modName] = true;
+        
+        function breakup(text, separator) {
+            return text.replace(/[A-Z]/g, function (match) {
+                return separator + match;
+            });
+        }
+        function enquote(str) {
+            return '\'' + str + '\'';
+        }
+        
+        var path = (name.substring(0, prefix.length + 1).toLowerCase() === prefix + '-') 
+                        ? name 
+                        : name.substring(0, prefix.length).toLowerCase() + "-" + lcwords(name.substring(prefix.length))
+        
+        var deps = dependenciesForModule(path);
+        
+        var src = [
+            'src/' + path + '/*.js',
+            '!src/' + path + '/*.docs.js'
+        ];
+        
+        var module = {
+            name: modName,
+            moduleName: enquote(modName),
+            displayName: modName,
+            srcFiles: grunt.file.expand({ filter: 'isFile' }, src),
+            cssFiles: grunt.file.expand('src/' + path + '/*.css'),
+            tplFiles: grunt.file.expand('template/' + path + '/*.html'),
+            tpljsFiles: grunt.file.expand('template/' + path + '/*.html.js'),
+            tplModules: grunt.file.expand('template/' + path + '/*.html').map(enquote),
+            dependencies: deps,
+            docs: {
+                md: grunt.file.expand('src/' + path + '/docs/*.md').map(grunt.file.read).map(markdown).join('\n'),
+                js: grunt.file.expand('src/' + path + '/docs/*.js').map(grunt.file.read).join('\n'),
+                html: grunt.file.expand('src/' + path + '/docs/*.html').map(grunt.file.read).join('\n')
+            }
+        };
+        
+        var styles = {
+            css: [],
+            js: []
+        };
+        module.cssFiles.forEach(processCSS.bind(null, styles, true));
+        if (styles.css.length) {
+            module.css = styles.css.join('\n');
+            module.cssJs = styles.js.join('\n');
+        }
+        
+        module.dependencies.forEach(findModule);
+        grunt.config('build.modules', grunt.config('build.modules').concat(module));
+    }
+    
+    function dependenciesForModule(name) {
+        var deps = [];
+        
+        var src = [
+            'src/' + name + '/*.js',
+            '!src/' + name + '/*.docs.js'
+        ];
+        
+        grunt.file.expand({ filter: 'isFile' }, src).map(grunt.file.read).forEach(function (contents) {
+            //Strategy: find where module is declared,
+            //and from there get everything inside the [] and split them by comma
+            
+            var moduleDecl = 'angular.module(';
+            var moduleDeclIndex = contents.indexOf(moduleDecl);
+            var hasModule = contents.substring(moduleDeclIndex + moduleDecl.length).indexOf(moduleDecl);
+            
+            if (hasModule) {
+                var moduleCode = contents.substring(moduleDeclIndex, hasModule - moduleDeclIndex);
+                
+                var depArrayStart = moduleCode.indexOf('[');
+                var depArrayEnd = moduleCode.indexOf(']', depArrayStart);
+                var dependencies = moduleCode.substring(depArrayStart + 1, depArrayEnd);
+                dependencies.split(',').forEach(function (dep) {
+                    var depName = dep.trim().replace(/['"]/g, '');
+                    if (depName.substring(0, modulePrefix.length).toLowerCase() === modulePrefix) {
+                        if (deps.indexOf(depName) < 0) {
+                            deps.push(depName);
+                            //Get dependencies for this new dependency
+                            deps = deps.concat(dependenciesForModule(depName));
+                        }
+                    }
+                });
+            }
+        });
+        return deps;
+    }
+    
+
+    function toAttribute(str) {
+        var sep = str.indexOf('-');
+        return (sep > -1) ? (str.substring(0, sep) + ucwords(str.substring(sep + 1))) : str;
+    }
+    
+    function ucwords(text) {
+        return text.replace(/^([a-z])|\s+([a-z])/g, function ($1) {
+            return $1.toUpperCase();
+        });
+    }
+    
+    function lcwords(text) {
+        return text.replace(/^([A-Z])|\s+([A-Z])/g, function ($1) {
+            return $1.toLowerCase();
+        });
+    }
+    
+    /**
+   * Logic from AngularJS
+   * https://github.com/angular/angular.js/blob/36831eccd1da37c089f2141a2c073a6db69f3e1d/lib/grunt/utils.js#L121-L145
+   */
+  function processCSS(state, minify, file) {
+        /* jshint quotmark: false */
+        var css = fs.readFileSync(file).toString(),
+            js;
+        state.css.push(css);
+        
+        if (minify) {
+            css = css
+        .replace(/\r?\n/g, '')
+        .replace(/\/\*.*?\*\//g, '')
+        .replace(/:\s+/g, ':')
+        .replace(/\s*\{\s*/g, '{')
+        .replace(/\s*\}\s*/g, '}')
+        .replace(/\s*\,\s*/g, ',')
+        .replace(/\s*\;\s*/g, ';');
+        }
+        //escape for js
+        css = css
+      .replace(/\\/g, '\\\\')
+      .replace(/'/g, "\\'")
+      .replace(/\r?\n/g, '\\n');
+        js = "!angular.$$csp() && angular.element(document).find('head').prepend('<style type=\"text/css\">" + css + "</style>');";
+        state.js.push(js);
+        
+        return state;
+    }
+};
